43e4ece319187fc003b1176a7243456e8f716cfa
mba.amaral <mba.amaral58@gmail.com>
2016-04-26 14:16:51 -0300

    Tela responsável pelo cadastro dos veículos que entram

--- PDS/src/Codigo/ControleDataeHora.java
+++ PDS/src/Codigo/ControleDataeHora.java
@@ -3,16 +3,23 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 
-public class Hora {
-	  public static void main(String[] args) {
-		  Date data = new Date();		 
-		  SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
-		  SimpleDateFormat horaFormatada = new SimpleDateFormat("hh:mm");
-		  String dataPronta =  dataFormatada.format(data);		  
-		  String horaPronta = horaFormatada.format(data);
-		  System.out.println("data "+dataPronta);
-		  System.out.println("hora "+horaPronta);
-		  
+public class ControleDataeHora {
+	
+	 Date data = new Date();		 
+	  SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+	  SimpleDateFormat horaFormatada = new SimpleDateFormat("hh:mm");
+	  String dataPronta =  dataFormatada.format(data);		  
+	  String horaPronta = horaFormatada.format(data);
+	public String getDataPronta() {
+		return dataPronta;
 	}
-
+	public void setDataPronta(String dataPronta) {
+		this.dataPronta = dataPronta;
+	}
+	public String getHoraPronta() {
+		return horaPronta;
+	}
+	public void setHoraPronta(String horaPronta) {
+		this.horaPronta = horaPronta;
+	}
 }
